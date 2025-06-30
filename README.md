9d018a06cef0560e891453df3611f7954c085905
Bedlessdeus <bedlesssgod@gmail.com>
2025-06-30 08:57:56 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# E1FI1CT Server
+
+Server side software for the Smarthome Project
+
+[Daemon](https://github.com/Bedlessdeus/E1FI1CT_Daemon)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	//import TempBar from "$lib/client/comp/TempBar.svelte";
+
+	export let data;
+</script>
+
+<div class="grid grid-cols-3 gap-4 p-8">
+	{#each data.items as item}
+		<a class="device" href={`/article/${item.id}`}>
+			<!--<TempBar current_temp={Math.round(Math.random() * 100)}/>-->
+			<div class="font-bold">{item.title}</div>
+			<div>{item.description}</div>
+		</a>
+	{/each}
+</div>
